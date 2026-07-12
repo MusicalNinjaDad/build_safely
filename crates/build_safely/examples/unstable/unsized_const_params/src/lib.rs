26eb328e3d3cbe219dfe251381c0f8c8854f0cf8
Mike Foster <i.am.mike.foster@gmail.com>
2026-07-12 15:04:25 +0000

    make the test meaningful

--- crates/build_safely/examples/unstable/unsized_const_params/src/lib.rs
+++ crates/build_safely/examples/unstable/unsized_const_params/src/lib.rs
@@ -1,14 +1,29 @@
 #![cfg_attr(unstable_unsized_const_params, feature(unsized_const_params))]
 #![cfg_attr(unstable_adt_const_params, feature(adt_const_params))]
 #![expect(incomplete_features)]
+#![allow(unused)]
+
+use std::fmt::Display;
+
+#[cfg(has_unsized_const_params)]
+struct Named<const NAME: &'static str>;
+
+#[cfg(has_unsized_const_params)]
+impl<const NAME: &'static str> Display for Named<NAME> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{NAME}")
+    }
+}
 
 #[cfg(test)]
 #[cfg(has_unsized_const_params)]
 mod has {
+    use super::*;
+
     #[test]
-    fn has() {
-        struct Foo<const N: &'static str>;
-        let _: Foo<"test">;
+    fn name() {
+        let bob = Named::<"Bob">;
+        assert_eq!("Bob", format!("{bob}"));
     }
 }
 
@@ -16,7 +31,7 @@ mod has {
 #[cfg(not(has_unsized_const_params))]
 mod has_not {
     #[test]
-    fn has_not() {
+    fn assert_true() {
         assert!(true);
     }
 }
