68b3d66cf92b3cebd0d306196e7f186cfe7e5b91
Mike Foster <i.am.mike.foster@gmail.com>
2026-07-12 15:38:56 +0000

    better test naming

--- crates/build_safely/tests/probes.rs
+++ crates/build_safely/tests/probes.rs
@@ -1,38 +1,95 @@
 use std::{path::PathBuf, process::Command};
 
+use derive_more::Display;
 use rstest::*;
 
-#[rstest]
-/// Runs the tests for each example under `examples/unstable`
-///
-/// All examples have 2 subdirs `allowed` & `forbidden`, each containing a `.cargo/config.toml` which
-/// either specifically allows or forbids the feature. This supports cases where one feature depends
-/// on others also being enabled (e.g. unsized_const_params, try_trait_v2).
-fn test_unstable(
-    #[files("*")]
-    #[dirs]
-    #[base_dir = "examples/unstable"]
-    example: PathBuf,
-    #[values(None, Some("allowed"), Some("forbidden"))] config: Option<&str>,
-) {
-    let mut test = Command::new("cargo");
-    test.arg("test").current_dir(&example);
-    match config {
-        None => {}
-        Some(config) => {
-            test.env("BUILD_SAFELY_CARGO_CONFIG_DIR", example.join(config));
-        }
+use Channel::*;
+
+struct Setup {
+    config_dir: Option<&'static str>,
+    channel: Channel,
+    suffix: Option<&'static str>,
+    has: bool,
+}
+
+#[derive(Display)]
+#[allow(non_camel_case_types)]
+enum Channel {
+    stable,
+    nightly,
+}
+
+mod unstable {
+    use super::*;
+
+    const NIGHTLY: Setup = Setup {
+        config_dir: None,
+        channel: nightly,
+        suffix: None,
+        has: true,
     };
-    let output = test.output().unwrap();
-    let stdout = String::from_utf8_lossy(&output.stdout);
-    match config {
-        Some("forbidden") => assert!(
-            stdout.contains("test has_not::"),
-            "incorrect tests run: {stdout}"
-        ),
-        _ => assert!(
-            stdout.contains("test has::"),
-            "incorrect tests run: {stdout}"
-        ),
+
+    const NIGHTLY_ALLOWED: Setup = Setup {
+        config_dir: Some("allowed"),
+        channel: nightly,
+        suffix: None,
+        has: true,
     };
+
+    const NIGHTLY_FORBIDDEN: Setup = Setup {
+        config_dir: Some("forbidden"),
+        channel: nightly,
+        suffix: None,
+        has: false,
+    };
+
+    #[rstest]
+    /// Runs the tests for each example under `examples/unstable`
+    ///
+    /// All examples have 2 subdirs `allowed` & `forbidden`, each containing a `.cargo/config.toml` which
+    /// either specifically allows or forbids the feature. This supports cases where one feature depends
+    /// on others also being enabled (e.g. unsized_const_params, try_trait_v2).
+    fn examples(
+        #[files("*")]
+        #[dirs]
+        #[base_dir = "examples/unstable"]
+        example: PathBuf,
+        #[values(NIGHTLY, NIGHTLY_ALLOWED, NIGHTLY_FORBIDDEN)] setup: Setup,
+    ) {
+        let Setup {
+            config_dir,
+            channel,
+            suffix,
+            has,
+        } = setup;
+
+        let toolchain = if let Some(suffix) = suffix {
+            format!("+{channel}{suffix}")
+        } else {
+            format!("+{channel}")
+        };
+
+        let mut test = Command::new("cargo");
+        test.args([&toolchain, "test"]).current_dir(&example);
+        match config_dir {
+            None => {}
+            Some(config) => {
+                test.env("BUILD_SAFELY_CARGO_CONFIG_DIR", example.join(config));
+            }
+        };
+        let output = test.output().unwrap();
+        let stdout = String::from_utf8_lossy(&output.stdout);
+
+        if has {
+            assert!(
+                stdout.contains("test has::"),
+                "incorrect tests run: {stdout}"
+            );
+        } else {
+            assert!(
+                stdout.contains("test has_not::"),
+                "incorrect tests run: {stdout}"
+            );
+        };
+    }
 }
